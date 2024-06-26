f4d00b51a4c6d16d959286daec1af28eb08ddd98
Allan Pedroni <abpedroni@stefanini.com>
2024-06-26 08:31:19 -0300

    33. We Need Custom Events! finish

--- essentials/01-starting-project/src/app/user/user.component.ts
+++ essentials/01-starting-project/src/app/user/user.component.ts
@@ -10,18 +10,12 @@ import { Component, Input, computed, input } from '@angular/core';
 })
 export class UserComponent {
   
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  })
-
-  // public get imagePath(): string {
-  //   return `assets/users/${this.avatar()}`;
-  // }
+  public get imagePath(): string {
+    return `assets/users/${this.avatar}`;
+  }
 
   onSelectUser() {
   }
