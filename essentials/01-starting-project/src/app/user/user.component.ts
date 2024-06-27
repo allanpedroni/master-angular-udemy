03496a5a814662604406a9574917a275373769f7
Allan Pedroni <abpedroni@stefanini.com>
2024-06-27 07:31:57 -0300

    35. Using the output() Function finish

--- essentials/01-starting-project/src/app/user/user.component.ts
+++ essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 //signal = allow angular to detect changes in the object
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,7 +13,8 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  //@Output() select = new EventEmitter();
+  select = output<string>();
 
   public get imagePath(): string {
     return `assets/users/${this.avatar}`;
