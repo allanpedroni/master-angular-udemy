91157e1e65427e622378e8a0d9d3ae8068b75377
Allan Pedroni <abpedroni@stefanini.com>
2024-06-28 08:13:34 -0300

    46. Outputting Task Data in the Task Component finish

--- essentials/01-starting-project/src/app/tasks/task/task.component.ts
+++ essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+type Task = {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+};
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-
+  @Input({required: true}) task?: Task;
 }
