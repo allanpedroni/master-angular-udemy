d24a1a72fe7fc194576442875674dab0231234ae
Allan Pedroni <abpedroni@stefanini.com>
2024-07-01 08:19:45 -0300

    56. Content Projection with ng-content finish

--- essentials/01-starting-project/src/app/tasks/task/task.component.ts
+++ essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,13 +1,14 @@
 import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
 
 @Component({
-  selector: 'app-task',
-  standalone: true,
-  imports: [DatePipe],
-  templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+    selector: 'app-task',
+    standalone: true,
+    templateUrl: './task.component.html',
+    styleUrl: './task.component.css',
+    imports: [DatePipe, CardComponent]
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task;
--- essentials/01-starting-project/src/app/user/user.component.ts
+++ essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,13 +1,14 @@
 //signal = allow angular to detect changes in the object
 import { Component, EventEmitter, Input, Output, input } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [CardComponent]
 })
 export class UserComponent {
 
