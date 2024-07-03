fda33fba6cf320c02c829229a87e0668d61b35e4
Allan Pedroni <abpedroni@stefanini.com>
2024-07-03 07:58:33 -0300

    62. Using localStorage for Data Storage finish

--- essentials/01-starting-project/src/app/tasks/tasks.service.ts
+++ essentials/01-starting-project/src/app/tasks/tasks.service.ts
@@ -50,6 +50,16 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks =
+      localStorage.getItem('tasks');
+      //JSON.parse(localStorage.getItem('tasks')!) : [];
+
+      if (tasks) {
+        this.tasks = JSON.parse(tasks);
+      }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter(task => task.userId === userId);
   }
@@ -62,10 +72,16 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.dueDate,
     });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
 }
