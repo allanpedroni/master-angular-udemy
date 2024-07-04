414c353f25ff8a21dcb0158ae138a4b1c13d08be
Allan Pedroni <abpedroni@stefanini.com>
2024-07-04 08:08:17 -0300

    70. Migrating All Components To Use Modules finish

--- essentials/01-starting-project/src/app/tasks/task/task.component.ts
+++ essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,15 +1,11 @@
-import { DatePipe } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { type Task } from './task.model';
-import { CardComponent } from "../../shared/card/card.component";
 import { TasksService } from '../tasks.service';
 
 @Component({
     selector: 'app-task',
-    standalone: true,
     templateUrl: './task.component.html',
     styleUrl: './task.component.css',
-    imports: [DatePipe, CardComponent]
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task;
