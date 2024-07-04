18213ebc8f42b41d88d80b64bc40653c001a0f26
Allan Pedroni <abpedroni@stefanini.com>
2024-07-04 08:25:54 -0300

    72. Creating More Complex Module-based App Structures finish

--- essentials/01-starting-project/src/app/app.module.ts
+++ essentials/01-starting-project/src/app/app.module.ts
@@ -4,22 +4,19 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { TasksComponent } from "./tasks/tasks.component";
 import { UserComponent } from "./user/user.component";
-import { CardComponent } from "./shared/card/card.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { TaskComponent } from "./tasks/task/task.component";
 import { SharedModule } from "./shared/shared.module";
+import { TaskModule } from "./tasks/tasks.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent],
-  imports: [BrowserModule, FormsModule, SharedModule],
+    UserComponent],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    TaskModule],
   providers: [],
   bootstrap: [AppComponent]
 })
