bb345fe310b7ba50819f07338b22b57ce8332cd4
Allan Pedroni <abpedroni@stefanini.com>
2024-07-04 07:57:50 -0300

    68. Declaring & Using Components finish

--- essentials/01-starting-project/src/app/app.module.ts
+++ essentials/01-starting-project/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { UserComponent } from "./user/user.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
