3712123040d49a744334671b4d04dbf62d91afbc
Allan Pedroni <abpedroni@stefanini.com>
2024-07-05 08:03:44 -0300

    78. Extracting Values with Two-Way-Binding finish

--- essentials/01-starting-project-investment/src/app/user-input/user-input.component.ts
+++ essentials/01-starting-project-investment/src/app/user-input/user-input.component.ts
@@ -9,6 +9,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredInitialInvestment: string = '0';
+  enteredAnnualInvestment: string = '0';
+  enteredExpectedReturn: string = '5';
+  enteredDuration: string = '10';
+
   onSubmit() {
     console.log('Form submitted');
   }
