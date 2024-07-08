571e54950188140c1ac849e122f6ce3d638f119a
Allan Pedroni <abpedroni@stefanini.com>
2024-07-08 08:04:27 -0300

    84. Formatting Output with a Pipe finish

--- essentials/01-starting-project-investment/src/app/investment-results/investment-results.component.ts
+++ essentials/01-starting-project-investment/src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, input } from '@angular/core';
 import { InvestmentResult } from '../investiment-input.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
