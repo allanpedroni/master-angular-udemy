1718133a282837f0865a64bbff0c4520ab5764a0
Allan Pedroni <abpedroni@stefanini.com>
2024-07-11 08:10:59 -0300

    102. Adding Forms to Components finish

--- essentials/01-starting-project-components-templates/src/app/app.component.ts
+++ essentials/01-starting-project-components-templates/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
-import { SupportTicketsComponent } from "./dashboard/support-tickets/support-tickets.component";
+import { SupportTicketsComponent } from "./dashboard/tickets/support-tickets.component";
 import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
