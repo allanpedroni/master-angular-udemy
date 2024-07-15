6e6bcaa870282648c930a3d5b28d6036295996b1
allan.pedroni@siteware.com.br <allan.pedroni@siteware.com.br>
2024-07-15 07:53:28 -0300

    113. Interacting With Host Elements From Inside Components finish

--- essentials/01-starting-project-components-templates/src/app/shared/control/control.component.ts
+++ essentials/01-starting-project-components-templates/src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import { Component, ViewEncapsulation, input } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None //disable shadow DOM encapsulation for this component
+  encapsulation: ViewEncapsulation.None, //NOTE: disable shadow DOM encapsulation for this component
+  host: {
+    class: 'control' //NOTE: add class to host element, in this case the component itself (app-control)
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
