d88377839d89890f207d24efea9cdef9f543d85c
allan.pedroni@siteware.com.br <allan.pedroni@siteware.com.br>
2024-07-15 08:16:40 -0300

    115. Interacting with Host Elements via @HostListener & @HostBinding finish

--- essentials/01-starting-project-components-templates/src/app/shared/control/control.component.ts
+++ essentials/01-starting-project-components-templates/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, input } from '@angular/core';
+import { Component, HostBinding, HostListener, ViewEncapsulation, input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,24 @@ import { Component, ViewEncapsulation, input } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, //NOTE: disable shadow DOM encapsulation for this component
   host: {
-    class: 'control' //NOTE: add class to host element, in this case the component itself (app-control)
+    class: 'control', //NOTE: add class to host element, in this case the component itself (app-control)
+    //'(click)': 'onClick()', //NOTE: add role attribute to host element
   }
 })
 export class ControlComponent {
+  //add a property to your component class
+  //just exists for backward compatibility, not used in this example.
+  //use host: { class: 'control' } instead
+  //@HostBinding('class') className = 'control';
+
+  //add an event listener to your component class
+  //just exists for backward compatibility, not used in this example.
+  //use host: { '(click)': 'onClick()' } instead
+  //@HostListener('click', ['onClick()'])
+
   label = input.required<string>();
+
+  // onClick() {
+  //   console.log('clicked!');
+  // }
 }
