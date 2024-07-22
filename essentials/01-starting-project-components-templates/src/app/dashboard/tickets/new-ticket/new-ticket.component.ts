c1a1e7f589c832ac1e77b80f886f68c0103822f6
abpedroni <allan.barros@gmail.com>
2024-07-22 07:40:32 -0300

    127. Working with Template Variables -

--- essentials/01-starting-project-components-templates/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ essentials/01-starting-project-components-templates/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit()  {
-    console.log('Form submitted');
+  onSubmit(titleInput: HTMLInputElement)  {
+    console.log('Form submitted' + titleInput);
   }
 }
