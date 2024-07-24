068f4640ec2a940d8b88b81bdd68eff42c03027f
abpedroni <allan.barros@gmail.com>
2024-07-24 08:13:22 -0300

    139. A Closer Look At Template For Loops finish

--- essentials/01-starting-project-components-templates/src/app/dashboard/tickets/support-tickets.component.ts
+++ essentials/01-starting-project-components-templates/src/app/dashboard/tickets/support-tickets.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { type Ticket } from './support-tickets.model';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-support-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './support-tickets.component.html',
   styleUrl: './support-tickets.component.css'
 })
 export class SupportTicketsComponent {
   tickets: Ticket[] = [];
-
 
   onAdd(ticketData: { title: string, text: string }) {
     const ticket: Ticket = {
