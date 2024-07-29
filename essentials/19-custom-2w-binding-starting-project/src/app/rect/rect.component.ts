49bfe8eb165ef12c45062b97eb68ef8a994dd3ae
abpedroni <allan.barros@gmail.com>
2024-07-29 07:45:34 -0300

    146. An Easier Way of Setting Up Custom Two-Way Binding finish

--- essentials/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ essentials/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,12 +9,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class RectComponent {
 
-  @Input({ required: true }) size!: { width: string, height: string };
-  @Output() sizeChange = new EventEmitter<{ width: string, height: string }>();
+  // @Input({ required: true }) size!: { width: string, height: string };
+  // @Output() sizeChange = new EventEmitter<{ width: string, height: string }>();
   //input.required<{width: string, height: string}>();
+  // substitute for @Input and @Output (two-way binding).
+  // only works with angular 17.2 or higher
+  size = model.required<{ width: string, height: string }>();
 
   onReset() {
-    this.sizeChange.emit({
+    this.size.set({
       width: '100',
       height: '100'
     });
