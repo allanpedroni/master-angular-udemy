98e3ede69503faa4fddc47bdc561c890d0ad2539
abpedroni <allan.barros@gmail.com>
2024-07-30 07:39:49 -0300

    151. Analyzing a Built-in Structural Directive: ngIf finish

--- essentials/07-directives-deep-dive/src/app/app.component.ts
+++ essentials/07-directives-deep-dive/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
 
+import { Component, computed } from '@angular/core';
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthService } from './auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent],
+  imports: [AuthComponent, LearningResourcesComponent, CommonModule],
 })
-export class AppComponent {}
+export class AppComponent {
+
+  isAdmin = computed(() => this.authService.activePermission() === 'admin');
+
+  constructor(private authService: AuthService) {
+    console.log('AppComponent created');
+  }
+
+
+}
