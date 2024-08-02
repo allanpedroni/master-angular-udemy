8b8f01d22d9a3f076d5cf3924a608cf514feab9b
abpedroni <allan.barros@gmail.com>
2024-08-02 07:46:59 -0300

    167. Chaining Pipes & Being Aware of Limitations finish

--- essentials/08-pipes-deep-dive/src/app/temperature.pipe.ts
+++ essentials/08-pipes-deep-dive/src/app/temperature.pipe.ts
@@ -1,24 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 |  exponentialStrength:10}}
- *   formats to: 1024
-*/
+
 @Pipe({
   name: 'temperature',
   standalone: true
 })
 export class TemperaturePipe implements PipeTransform {
-  //transform(value: string, ...args: any): string {
   transform(
-    value: string | number,
-    inputType: 'C' | 'F',
-    outputType: 'C' | 'F'): string {
+    value: string | number | null,
+    inputType: 'C' | 'F' = 'C',
+    outputType?: 'C' | 'F') {
 
+    if (outputType === undefined) {
+      outputType = inputType;
+    }
+
+    if (!value) {
+      return value;
+    }
     if (typeof value === 'string') {
       value = parseFloat(value);
     }
@@ -28,6 +26,6 @@ export class TemperaturePipe implements PipeTransform {
     } else if (inputType === 'C') {
       value = value * 9 / 5 + 32;
     }
-    return `${value} °${outputType}`;
+    return `${value.toFixed(2)} °${outputType}`;
   }
 }
