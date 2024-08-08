9038e55648e9f38e0f4526ee6a60ee47e65c14be
abpedroni <allan.barros@gmail.com>
2024-08-08 07:40:52 -0300

    181. Providing Services via the Element Injector finish

--- essentials/09-services-deep-dive/src/app/tasks/tasks.component.ts
+++ essentials/09-services-deep-dive/src/app/tasks/tasks.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
+  //all the components that are used in this component like NewTaskComponent and TasksListComponent will have access to the TasksService
+  providers: [TasksService],
 })
 export class TasksComponent {}
