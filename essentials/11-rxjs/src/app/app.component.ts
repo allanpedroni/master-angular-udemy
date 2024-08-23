29a4a52611dd0ba90f218289261cee4a6618b66d
abpedroni <allan.barros@gmail.com>
2024-08-23 08:01:48 -0300

    208. Working with Signals finish

--- essentials/11-rxjs/src/app/app.component.ts
+++ essentials/11-rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 
-import { interval, map, pipe } from 'rxjs'
+import { interval, map } from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -9,25 +9,36 @@ import { interval, map, pipe } from 'rxjs'
 })
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
+  clickCount = signal(0);
+
+  constructor() {
+    effect(() => {
+      console.log('Click count changed to ' + this.clickCount());
+    });
+  }
 
   ngOnInit(): void {
-    //every second, log a message to the console
-    const subscriptionInterval =
-      interval(1000)
-      .pipe(
-        map((value) => value * 2)
-      )
-      .subscribe({
-        next: (val) => { console.log(`I am a standalone component: ${val}`); },
-        complete: () => { console.log('This is how you can create a standalone component in Angular'); },
-        error: (err) => {
-          console.error('An error occurred:', err);
-        },
-      });
+    // //every second, log a message to the console
+    // const subscriptionInterval =
+    //   interval(1000)
+    //     .pipe
+    //     (
+    //       map((value) => value * 2)
+    //     )
+    //     .subscribe
+    //     ({
+    //       next: (val) => { console.log(`I am a standalone component: ${val}`); },
+    //       complete: () => { console.log('This is how you can create a standalone component in Angular'); },
+    //       error: (err) => { console.error('An error occurred:', err); },
+    //     });
 
-    //destroy the subscription when the component is destroyed
-    this.destroyRef.onDestroy(() => {
-      subscriptionInterval.unsubscribe();
-    });
+    // //destroy the subscription when the component is destroyed
+    // this.destroyRef.onDestroy(() => {
+    //   subscriptionInterval.unsubscribe();
+    // });
+  }
+
+  onClick() {
+    this.clickCount.update(previuesValue => previuesValue + 1);
   }
 }
