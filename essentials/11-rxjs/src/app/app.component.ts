2b6c72c4527f4499b8d3902b4663feaddb97a384
abpedroni <allan.barros@gmail.com>
2024-08-23 07:55:50 -0300

    207. Working with RxJS Operators finish

--- essentials/11-rxjs/src/app/app.component.ts
+++ essentials/11-rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
-import { interval } from 'rxjs'
+import { interval, map, pipe } from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -12,13 +12,18 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     //every second, log a message to the console
-    const subscriptionInterval = interval(1000).subscribe({
-      next: () => { console.log('I am a standalone component'); },
-      complete: () => { console.log('This is how you can create a standalone component in Angular'); },
-      error: (err) => {
-        console.error('An error occurred:', err);
-      },
-    });
+    const subscriptionInterval =
+      interval(1000)
+      .pipe(
+        map((value) => value * 2)
+      )
+      .subscribe({
+        next: (val) => { console.log(`I am a standalone component: ${val}`); },
+        complete: () => { console.log('This is how you can create a standalone component in Angular'); },
+        error: (err) => {
+          console.error('An error occurred:', err);
+        },
+      });
 
     //destroy the subscription when the component is destroyed
     this.destroyRef.onDestroy(() => {
